611154d7d47da263710ff393115c802438307d3d
norrthh <nortmuller92@gmail.com>
2024-11-01 06:44:49 +0300

    Ваше сообщение2

--- src/components/Admins/AdminDashboardComponent.vue
+++ src/components/Admins/AdminDashboardComponent.vue
@@ -2,68 +2,56 @@
 import {ref} from "vue";
 import Korobka from "@/components/Admins/Event/Korobka.vue";
 
-let selectPage = ref()
+const selectPage = ref('DEFAULT')
 
 const changePage = (page) => {
-    selectPage.value = page
+  selectPage.value = page
+  console.log("selectPage:", selectPage.value, typeof selectPage.value);
 }
 
 </script>
 
 <template>
-    <section class="flex flex-col w-full gap-12" v-if="!selectPage">
-        <div class="flex flex-col w-full gap-2 items-center">
-            <h1 class="text-3xl font-black">УПРАВЛЕНИЕ КОНКУРСАМИ</h1>
+  <section class="flex flex-col w-full gap-12" v-if="selectPage === 'DEFAULT'">
+    <div class="flex flex-col w-full gap-2 items-center">
+      <h1 class="text-3xl font-black">ПАНЕЛЬ АДМИНИСТРАТОРА</h1>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full font-black">
+      <div
+          class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
+          @click="changePage('eventZ')">
+        <div class="flex items-center gap-4">
+          <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
+                                                            class="h-[18px]"></div>
+          <h2 class="">УПРАВЛЕНИЕ МЕРОПРИЯТИЯМИ</h2>
         </div>
+        <img src="/ayazik/arrow-right.svg" alt="">
+      </div>
+    </div>
+  </section>
 
-        <div class="flex flex-col gap-4 w-full font-black">
-            <div
-                class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
-                @click="changePage('korobka')">
-                <div class="flex items-center gap-4">
-                    <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
-                                                                      class="h-[18px]"></div>
-                    <h2 class="">КОРОБКА С СЮРПРИЗОМ</h2>
-                </div>
-                <img src="/ayazik/arrow-right.svg" alt="">
-            </div>
+  <section class="flex flex-col w-full gap-12" v-if="selectPage === 'eventZ'">
+    <div class="flex flex-col w-full gap-2 items-center">
+      <h1 class="text-3xl font-black">ПАНЕЛЬ АДМИНИСТРАТОРА</h1>
+    </div>
 
-            <div
-                class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
-                @click="changePage('bonus')">
-                <div class="flex items-center gap-4">
-                    <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
-                                                                      class="h-[18px]"></div>
-                    <h2 class="">РУЛЕТКА</h2>
-                </div>
-                <img src="/ayazik/arrow-right.svg" alt="">
-            </div>
-
-            <div
-                class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
-                @click="changePage('bonus')">
-                <div class="flex items-center gap-4">
-                    <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
-                                                                      class="h-[18px]"></div>
-                    <h2 class="">УГАДАЙ ГДЕ ПРИЗ</h2>
-                </div>
-                <img src="/ayazik/arrow-right.svg" alt="">
-            </div>
-
-            <div
-                class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
-                @click="changePage('bonus')">
-                <div class="flex items-center gap-4">
-                    <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
-                                                                      class="h-[18px]"></div>
-                    <h2 class="">ПРОМОКОДЫ</h2>
-                </div>
-                <img src="/ayazik/arrow-right.svg" alt="">
-            </div>
+    <div class="flex flex-col gap-4 w-full font-black">
+      <div
+          class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between"
+          @click="changePage('korobka')">
+        <div class="flex items-center gap-4">
+          <div class="bg-[#FFFFFF0F] rounded-full p-4"><img src="/ayazik/gift.svg" alt=""
+                                                            class="h-[18px]"></div>
+          <h2 class="">КОРОБКА С СЮРПРИЗОМ</h2>
         </div>
-    </section>
+        <img src="/ayazik/arrow-right.svg" alt="">
+      </div>
+    </div>
+  </section>
 
-    <Korobka v-if="selectPage === 'korobka'" />
+
+  <Korobka v-if="selectPage === 'korobka'"/>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import bridge from '@vkontakte/vk-bridge';
 import './index.css'
+import 'flowbite';
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
