c789fa700899ac1e9f96cc31d38ab04828f0bdd1
norrthh <nortmuller92@gmail.com>
2024-11-19 14:11:23 +0300

    fix cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,11 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
       $middleware->web(append: [
          \App\Http\Middleware\HandleInertiaRequests::class,
          \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-         \App\Http\Middleware\OwnCors::class
-      ]);
-
-      $middleware->api(append: [
-         \App\Http\Middleware\OwnCors::class
       ]);
 
       $middleware->validateCsrfTokens([
