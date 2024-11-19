8cb53456b1beb1f576b6343fedb893d41dd935f4
norrthh <nortmuller92@gmail.com>
2024-11-19 12:58:37 +0300

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'paths' => ['*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
+   'paths' => ['api/*', 'sanctum/csrf-cookie'],
+   'allowed_methods' => ['*'],
+   'allowed_origins' => ['*'],
+
+   'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
