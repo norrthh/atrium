10e47b566bf8779c131c1efb5c5a9b95532709dd
norrthh <nortmuller92@gmail.com>
2024-11-20 19:39:58 +0300

    currect settings

--- resources/js/Components/ActivityComponent.vue
+++ resources/js/Components/ActivityComponent.vue
@@ -1,18 +1,25 @@
 <script setup>
-    defineProps({
-        activity: {
-            type: Object
-        }
-    })
+import { computed } from 'vue';
+
+defineProps({
+   activity: {
+      type: Array,
+      default: () => []
+   }
+})
+
+// Вычисляемое свойство для сортировки массива по убыванию coin
+const sortedActivities = computed(() => {
+   return [...activity].sort((a, b) => b.coin - a.coin);
+})
 </script>
 
 <template>
-   <div v-if="activity.length > 0" v-for="(activity, i) in activity">
-      <div class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between" :class="{'rat1': i === 0, 'rat2': i === 1, 'rat3': i === 2, 'rat4': i > 2}"
-      >
+   <div v-if="sortedActivities.length > 0" v-for="(activity, i) in sortedActivities" :key="i">
+      <div class="flex gap-4 items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between" :class="{'rat1': i === 0, 'rat2': i === 1, 'rat3': i === 2, 'rat4': i > 2}">
          <div class="flex items-center gap-4">
             <h2 class="font-black uppercase">#{{ i + 1 }} </h2>
-            <div class="rounded-full  overflow-hidden">
+            <div class="rounded-full overflow-hidden">
                <img :src="activity.avatar ? activity.avatar : '/ayazik/no_image.png'" alt="" class="h-12">
             </div>
             <h2 class="font-black uppercase">{{ activity.nickname }}</h2>
@@ -26,6 +33,6 @@
       <p class="text-center text-2xl">Ничего не найдено</p>
    </div>
 </template>
-<style scoped>
 
+<style scoped>
 </style>
