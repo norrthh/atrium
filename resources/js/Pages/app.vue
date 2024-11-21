866e4adeba3359f552a829d4c998df8732dec3c4
norrthh <nortmuller92@gmail.com>
2024-11-21 16:30:37 +0300

    add button copy code

--- resources/js/Pages/app.vue
+++ resources/js/Pages/app.vue
@@ -633,10 +633,6 @@ const transferActivateCode = () => {
       to: 'vkontakte',
    }).then(res => {
       transferResponse.value = res.data
-
-      if (res.data.status) {
-         window.location.reload();
-      }
    })
 }
 
@@ -1253,7 +1249,6 @@ const transferCopyToClipboard = () => {
                      </div>
 
                      <div v-if="!user.connect_social">
-
                         <div
                            class="flex gap-4 font-black items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between mt-4"
                            @click="transferModal(0)">
@@ -1266,6 +1261,22 @@ const transferCopyToClipboard = () => {
                               <div class="flex flex-col uppercase">
                                  <span>ПРИВЯЗАТЬ ВК АККАУНТ</span>
                                  <span style="color: #FFFFFFA3;">НЕ ПРИВЯЗАН</span>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div v-else>
+                        <div
+                           class="flex gap-4 font-black items-center bg-[#FFFFFF0F] p-4 rounded-full text-white justify-between mt-4">
+                           <div class="flex items-center gap-4">
+                              <div class="bg-[#FFFFFF0F] rounded-full p-4">
+                                 <img
+                                    src='/ayazik/icons/VK.svg'
+                                    alt="">
+                              </div>
+                              <div class="flex flex-col uppercase">
+                                 <span>ПРИВЯЗАТЬ ВК АККАУНТ</span>
+                                 <span style="color: #FFFFFFA3;">ПРИВЯЗАН</span>
                               </div>
                            </div>
                         </div>
