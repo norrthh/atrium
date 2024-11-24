cacca0966f0a3f5cd44ec1547c3a937d69e99c0a
norrthh <nortmuller92@gmail.com>
2024-11-25 02:44:54 +0300

    currect

--- app/Telegraph/TelegraphHandler.php
+++ app/Telegraph/TelegraphHandler.php
@@ -10,11 +10,17 @@ use App\Models\User\User;
 use App\Models\UserTask;
 use App\Services\Telegram\TelegramMethodServices;
 use App\Telegraph\Message\TelegraphMessage;
+use DefStudio\Telegraph\DTO\CallbackQuery;
+use DefStudio\Telegraph\DTO\InlineQuery;
+use DefStudio\Telegraph\DTO\Message;
+use DefStudio\Telegraph\DTO\Reaction;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use DefStudio\Telegraph\Keyboard\Keyboard;
+use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Stringable;
+use Illuminate\Http\Request;
 
 class TelegraphHandler extends WebhookHandler
 {
@@ -28,12 +34,11 @@ class TelegraphHandler extends WebhookHandler
       } else {
          if ($this->message->from()->username() == '') {
             $this->chat->message('У вас должен быть установлен username в настройках, чтобы запустить приложение')->send();
-         } else {
-            $this->chat->message(print_r($subscription, 1))->send();
+         } else
             $this->chat->message('Вы должны подписаться на телеграмм канал @atriumru, чтобы продолжить дальше')->send();
-         }
       }
    }
+
    public function handleChatMessage(Stringable $text): void
    {
       Log::info('reply0');
@@ -47,7 +52,7 @@ class TelegraphHandler extends WebhookHandler
          $task = Tasks::query()->where([['social_id', $this->message->chat()->id()], ['status', 0], ['typeSocial', 2], ['typeTask', '2']])->first();
          if ($task) {
             if ($task->access['type'] == 1 and Carbon::parse($task->created_at)->diffInMinutes(now()) >= $task->access['value']) {
-               return ;
+               return;
             } else {
                if (!UserTask::query()->where([['task_id', $task->id], ['user_id', $member->id()]])->exists()) {
                   UserTask::query()->create([
@@ -61,6 +66,63 @@ class TelegraphHandler extends WebhookHandler
                }
             }
          }
+      }
+   }
+
+   public function handle(Request $request, TelegraphBot $bot): void
+   {
+      try {
+         $this->bot = $bot;
+
+         $this->request = $request;
+
+         Log::info(print_r($request->all(), 1));
+
+         if ($this->request->has('message')) {
+            /* @phpstan-ignore-next-line */
+            $this->message = Message::fromArray($this->request->input('message'));
+            $this->handleMessage();
+
+            return;
+         }
+
+         if ($this->request->has('edited_message')) {
+            /* @phpstan-ignore-next-line */
+            $this->message = Message::fromArray($this->request->input('edited_message'));
+            $this->handleMessage();
+
+            return;
+         }
+
+         if ($this->request->has('channel_post')) {
+            /* @phpstan-ignore-next-line */
+            $this->message = Message::fromArray($this->request->input('channel_post'));
+            $this->handleMessage();
+
+            return;
+         }
+
+         if ($this->request->has('message_reaction')) {
+            /* @phpstan-ignore-next-line */
+            $this->reaction = Reaction::fromArray($this->request->input('message_reaction'));
+            $this->handleReaction();
+
+            return;
+         }
+
+
+         if ($this->request->has('callback_query')) {
+            /* @phpstan-ignore-next-line */
+            $this->callbackQuery = CallbackQuery::fromArray($this->request->input('callback_query'));
+            $this->handleCallbackQuery();
+         }
+
+         if ($this->request->has('inline_query')) {
+            /* @phpstan-ignore-next-line */
+            $this->handleInlineQuery(InlineQuery::fromArray($this->request->input('inline_query')));
+         }
+      } catch (Throwable $throwable) {
+         $this->onFailure($throwable);
       }
    }
 }
