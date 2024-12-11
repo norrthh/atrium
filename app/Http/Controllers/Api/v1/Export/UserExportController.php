4c8a8f864e670e3898767dde72e98c3e8bcc21e9
norrthh <nortmuller92@gmail.com>
2024-12-11 03:19:46 +0300

    fix

--- app/Http/Controllers/Api/v1/Export/UserExportController.php
+++ app/Http/Controllers/Api/v1/Export/UserExportController.php
@@ -12,6 +12,14 @@ class UserExportController extends Controller
 {
    public function index()
    {
-      return Excel::download(new UsersExport, 'users.xlsx');
+      $response = Excel::download(new UsersExport, 'users.xlsx');
+
+      // Установим дополнительные заголовки
+      $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+      $response->headers->set('Pragma', 'no-cache');
+      $response->headers->set('Expires', '0');
+
+      return $response;
    }
 }
