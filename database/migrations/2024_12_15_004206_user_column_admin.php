90b05193382f381a076e781dfd02933c76807888
norrthh <nortmuller92@gmail.com>
2024-12-15 00:58:53 +0300

    fix

--- database/migrations/2024_12_15_004206_user_column_admin.php
+++ database/migrations/2024_12_15_004206_user_column_admin.php
@@ -4,25 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->integer('admin')->default(0);
-        });
-    }
+return new class extends Migration {
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::table('users', function (Blueprint $table) {
+         $table->integer('admin')->default(0);
+      });
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('admin');
-        });
-    }
+      $users = [582127671, 217199523, 477629325, 232600787];
+
+      foreach ($users as $user) {
+          \App\Models\User\User::query()->where('vkontakte_id', $user)->update([
+            'admin' => 1
+          ]);
+      }
+   }
+
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::table('users', function (Blueprint $table) {
+         $table->dropColumn('admin');
+      });
+   }
 };
